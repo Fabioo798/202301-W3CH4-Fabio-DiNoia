49923be61904be2ce40047f13fcd5af015461c68
Fabio <fabiodn.798@gmail.com>
2023-02-02 16:55:48 +0100

    Solving sonar problem on pull request

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import { Footer } from "./components/footer/footer";
-import { Header } from "./components/header/header";
-import { List } from "./components/list/list";
-import './index.scss';
-import { PET } from "./mock/pets";
-
-
-
-new Header('.app');
-new Footer('.app');
-new List('main', PET);
+// main typescript file
